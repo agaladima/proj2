049f8433cd324f9be390c513b258bd341a971712
agaladima <arum.galadima@gmail.com>
2017-07-31 09:16:25 -0400

    Final commit
    finale

---
+++ pag-filter-v2/js/script.js
@@ -0,0 +1,98 @@
+//showpage function that displays a list of ten students based on "page number" user has selected//
+function showPage(page, list) {
+    // hide all students on the page
+    $(list).hide();
+
+    //beginning and ending position of page
+    var start = (page*10)-10;
+    var end = start + 10;
+    // Then loop through all students in student list argument
+    for (var i = 0; i < list.length; i++) {
+        // if student should be on this page
+        if (i < page*10 && i >= (page*10)-10) {
+            //show student
+            list.slice(start, end).show();
+        };
+    };
+}
+
+//appendpagelinks this creates links to different pages or lists of students//
+function appendPageLinks(list) {
+    // determine how many pages for this student list 
+    var pages = Math.ceil(list.length/10);
+    // create a page link section
+    var link = '';
+    var pageArr = [];
+    // “for” every page
+    for (var i = 1; i <= pages; i++) {
+        // add a page link to the page link section
+        link += '<li><a href="#">' + i + '</a></li>';
+        pageArr.push(i);
+    };
+        
+    // remove the old page link section from the site
+    $('.pagination ul li').remove();
+    // append our new page link section to the site
+    $('.pagination ul').append(link);
+    // define what happens when you click a link
+    $('.pagination ul li').click(function() {
+        // Use the showPage function to display the page for the link clicked
+        showPage($(this).text(),list);
+        // mark that link as “active”
+        $('.pagination ul li').removeClass('active');
+        $(this).addClass('active');
+    });     
+}
+
+function searchList() {
+    // Obtain the value of the search input
+    var result = $('input').val();
+    
+    // remove the previous page link section
+    $('.pagination ul li').remove();
+
+    //store matched results
+    var matched = [];
+    // Loop over the student list, and for each student…
+    $('.student-item').each(function(student) {
+        // ...obtain the student’s name…
+        var name = $(this).children('div').find('h3').text();
+        // ...and the student’s email…
+        var email = $(this).children('div').find('span').text();
+        //console.log(name + ' ' + email);
+
+        // ...if the search value is found inside either email or name…
+        if (name.includes(result) || email.includes(result)) {
+            // ...add this student to list of “matched” student
+            matched.push($(this));
+            console.log(matched);
+        }        
+    });
+
+    // If there’s no “matched” students…
+    if (matched.length === 0) {
+        // ...display a “no student’s found” message
+            alert("No student's found");
+        }      
+    // If over ten students were found…
+    if (matched.length > 10) {
+        appendPageLinks(matched);
+    };
+
+   // Call showPage to show first ten students of matched list
+   $('.pagination ul li').click(function() {
+        // Use the showPage function to display the page for the link clicked
+         showPage($(this).text(), matched);
+          // mark that link as “active”
+        $('.pagination ul li').removeClass('active');
+        $(this).addClass('active');
+    });   
+}
+
+//var list = $('.student-item');
+showPage(1, $('.student-item'));
+appendPageLinks($('.student-item'));
+$('button').click(function() {
+    searchList();
+});
+
